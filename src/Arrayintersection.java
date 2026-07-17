bdae027483e6b7b66cbf8908f60d13680b116683
Soham <kolhesoham3000@gmail.com>
2026-07-17 18:02:24 +0530

    Find intersection between two arrays

---
+++ src/Arrayintersection.java
@@ -0,0 +1,22 @@
+public class Arrayintersection {
+    static int[] Inter(int[] arr, int[] brr){
+        int[] ans = new int[arr.length];
+        for (int i=0 ; i< arr.length ; i++){
+            for (int j=0 ; j< brr.length ; j++){
+                if (arr[i]==brr[j]){
+                    ans[i]=arr[i];
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,4,3,5,6,7};
+        int[] brr = {1,4,5,9,10};
+        int[] answer = Inter(arr,brr);
+        for (int i : answer){
+            System.out.println(i);
+        }
+    }
+}
