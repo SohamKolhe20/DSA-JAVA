9487d7bee2ca786add48fc32f49f7d962e39aa38
Soham <kolhesoham3000@gmail.com>
2026-07-17 19:11:41 +0530

    Minor changes

---
+++ src/Arrays/Basics/ReverseArray.java
@@ -0,0 +1,25 @@
+package Arrays.Basics;
+
+public class ReverseArray {
+
+    static void reverse(int[] arr){
+        int n = arr.length;
+        int i = 0;
+        int j = n-1;
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        for (int ans : arr){
+            System.out.print(ans + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        reverse(arr);
+    }
+}
