42825f29b202db5fca0ebbd0bebd2b87d6319802
Soham <kolhesoham3000@gmail.com>
2026-07-17 13:11:16 +0530

    New basic problems

---
+++ src/Maximumofarray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Maximumofarray {
+    public static void main(String[] args) {
+        System.out.println("Maximum of array");
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array provided");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("--------------------------------------");
+        int mm = arr[0];
+        for (int i = 0; i < 5; i++) {
+            if (arr[i] > mm) {
+                mm = arr[i];
+            }
+        }
+        System.out.println("Max element is = " + mm);
+
+    }
+}
