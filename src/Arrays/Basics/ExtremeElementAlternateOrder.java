33255743113c547625207aac8ddb011e56ad0a8c
Soham <kolhesoham3000@gmail.com>
2026-07-18 15:42:34 +0530

    Shift array by K position problem

---
+++ src/Arrays/Basics/ExtremeElementAlternateOrder.java
@@ -0,0 +1,23 @@
+
+// Print Extreme Elements in an Alternate Manner.
+
+package Arrays.Basics;
+
+public class ExtremeElementAlternateOrder {
+    static void alternate(int[] arr){
+        int n = arr.length;
+        int j = n-1;
+        int i = 0;
+        while(i<j){
+            System.out.print(arr[i] + " ");
+            System.out.print(arr[j] + " ");
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6};
+        alternate(arr);
+    }
+}
