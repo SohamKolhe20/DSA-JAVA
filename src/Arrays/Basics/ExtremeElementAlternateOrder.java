554ae6dd3ba977c893e5673df22b386418cff8bf
Soham <kolhesoham3000@gmail.com>
2026-07-18 15:42:59 +0530

    Extreme Elements in an Alternate Manner

--- src/Arrays/Basics/ExtremeElementAlternateOrder.java
+++ src/Arrays/Basics/ExtremeElementAlternateOrder.java
@@ -5,7 +5,7 @@ package Arrays.Basics;
 
 public class ExtremeElementAlternateOrder {
     static void alternate(int[] arr){
-        int n = arr.length;
+        int n = arr.length:
         int j = n-1;
         int i = 0;
         while(i<j){
