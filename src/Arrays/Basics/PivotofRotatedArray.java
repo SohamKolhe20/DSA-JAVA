be06374d1774abb79e6a5e181e8aface29093174
Soham <kolhesoham3000@gmail.com>
2026-07-31 23:24:42 +0530

    Pivot index of the Sorted Rotated Array (Incomplete)

---
+++ src/Arrays/Basics/PivotofRotatedArray.java
@@ -0,0 +1,29 @@
+package Arrays.Basics;
+
+public class PivotofRotatedArray {
+    static int rotated(int[] arr){
+        int n = arr.length;
+        int s = 0;
+        int e = n-1;
+        int index=-1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if(arr[mid]>= arr[e]){
+                s = mid+1;
+            }
+            else {
+                e = mid-1;
+                index = mid;
+            }
+        }
+        return index;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {60,70,80,90,10,20,30,40,50};
+        int n = arr.length;
+        System.out.println(rotated(arr));
+    }
+}
