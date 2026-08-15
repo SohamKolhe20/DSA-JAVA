b88e183a118300b5d05cb02a1f05970fadc19842
Soham <kolhesoham3000@gmail.com>
2026-08-15 17:56:55 +0530

    Search in 2D matrix II - using Binary search

---
+++ src/Array/MediumHard/Search2DMatrix2.java
@@ -0,0 +1,30 @@
+package Array.MediumHard;
+
+public class Search2DMatrix2 {
+    static boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int r = 0;
+        int c = col-1;
+
+        while( r< row && c >=0){
+            if(matrix[r][c] == target){
+                return true;
+            }
+            else if(matrix[r][c] >=target){
+                c--;
+            }
+            else{
+                r++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int target = 9;
+        System.out.println(searchMatrix(matrix,target));
+    }
+}
+
