16ff53168def2a2203ffc8bac4afb12f678fff2b
Soham <kolhesoham3000@gmail.com>
2026-08-15 17:58:40 +0530

    Search in 2D matrix II - using Binary search

--- src/Array/MediumHard/Search2DMatrix2.java
+++ src/Array/MediumHard/Search2DMatrix2.java
@@ -1,5 +1,13 @@
-package Array.MediumHard;
+//Write an efficient algorithm that searches for a value target in an m x n integer matrix
+//This matrix has the following properties:
+//Integers in each row are sorted in ascending from left to right.
+//Integers in each column are sorted in ascending from top to bottom.
+//Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],
+// target = 5
+//Output: true
 
+
+package Array.MediumHard;
 public class Search2DMatrix2 {
     static boolean searchMatrix(int[][] matrix, int target) {
         int row = matrix.length;
